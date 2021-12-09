a4c3556ab2479262f0df748f0fdcd9f9743e5a29
itrinia <94580529+itrinia@users.noreply.github.com>
2021-12-09 14:54:38 +0700

    Add files via upload

---
+++ src/storyline1.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ileene
+ */
+public class storyline1 {
+    public static void main(String[] args) {
+        int opt1, opt2;
+        int menu;
+        String[] pil1 = {"Aku merasakan aku menjadi seorang mega bintang di usiaku saat ini"};
+        
+        System.out.println("Siapa yang tidak menantikan liburan?\n "
+                + "Begitu pula dengan aku, seorang pelajar yang sedang duduk di bangku SD kelas 5.\n"
+                + "Teman-temanku mengenalku dengan nama Sally Chou.\n"
+                + "Hampir satu sekolah ini mengenalku karena aku anak yang cantik dan berbakat.\n"
+                + "Namun, dibalik ketenaran yang aku miliki, aku hanyalah manusia biasa.\n"
+                + "Inilah kisahku.");
+        System.out.println("");
+        System.out.println("Di suatu hari liburan, aku tertidur pulas dalam mimpiku.");
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("");
+     
+    }
+}
