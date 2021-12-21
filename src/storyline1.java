1e0c7b9086417337671b149a6deb2d2d418826ad
itrinia <isantoso@student.ciputra.ac.id>
2021-12-22 00:42:51 +0700

    pilihan 1 (sh23)
    otw kelar pilihan 1 nya

--- src/storyline1.java
+++ src/storyline1.java
@@ -14,7 +14,6 @@ public class storyline1 {
 
     static Scanner scan = new Scanner(System.in);
     static storyline1 story = new storyline1();
-    static String storybegin;
 
     public storyline1() {
         System.out.println("\t\t===============================\n"
@@ -23,7 +22,7 @@ public class storyline1 {
 
         System.out.println("!!! Disclaimer: Cerita ini hanya fiksi belaka,\n"
                 + "tidak ada maksud terselubung dari cerita ini,\n"
-                + "dan mohon maaf apabila ada kesamaan nama/kejadian\n"
+                + "dan mohon maaf apabila ada kesamaan nama/tempat kejadian perkara\n"
                 + "karena ini disetting sedemikian rupa agar menjadi cerita fiksi.\n\n");
 
         System.out.println("! POINT OF VIEW: reader as Sally Chou.\n");
@@ -31,10 +30,43 @@ public class storyline1 {
         System.out.println("Pada suatu hari libur, ada anak cantik bernama Sally Chou.\n"
                 + "Ia tertidur pulas.\n"
                 + "Dalam mimpinya, ia bermimpi pergi ke suatu tempat luas.\n"
-                + "Tempat itu sangat gelap, namun ia menemukan dua ruangan.\n"
-                + "Ruangan pertama bertuliskan 'YK89'.\n"
-                + "Ruang kedua bertuliskan 'CS10'.");
-        System.out.println("Apakah kamu mau ");
+                + "Tempat itu sangat gelap, namun ia menemukan dua ruangan.");
+
+        String ruang;
+        do {
+            System.out.println("Ruangan pertama bertuliskan 'SH03'."
+                    + "\nRuangan kedua bertuliskan 'CS10'."
+                    + "\nApakah kamu mau pergi ke ruang pertama atau kedua?");
+            System.out.print("ketik 'SH03' atau 'CS10': ");
+            ruang = scan.nextLine();
+            if (ruang.equalsIgnoreCase("SH03")) {
+                sh03();
+            } else if (ruang.equalsIgnoreCase("CS10")) {
+                cs10();
+            } else {
+                System.out.println("inputan anda tidak valid. silakan input ulang\n");              //masih ngeloop mulu klo inputan udah bner
+            }
+        } while (ruang != "SH03" || ruang != "CS10");
+    }
+
+    public void sh03() {
+        String box;
+        System.out.println("\nkamu melihat box hitam.");
+        do {
+            System.out.print("ketik 'buka' atau 'biarkan': ");
+            box = scan.nextLine();
+            if (box.equalsIgnoreCase("buka")) {
+                System.out.println("\nkamu menemukan tulisan 'happy'");
+            } else if (box.equalsIgnoreCase("biarkan")) {
+                System.out.println("\nkamu berjalan lagi dan menemukan box biru.");
+            } else {
+                System.out.println("tolong input sesuai yang diminta.");
+            }
+        } while (box != "buka" || box != "biarkan");
+    }
+
+    public void cs10() {
+
     }
 
     public static void main(String[] args) {
