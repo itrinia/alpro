16d1f5931dae3254c46a7fd81c85c9a7315ab703
itrinia <isantoso@student.ciputra.ac.id>
2021-12-21 23:39:04 +0700

    revisi method 1
    revisi method public storyline

--- src/storyline1.java
+++ src/storyline1.java
@@ -6,27 +6,28 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Ileene
  */
 public class storyline1 {
-    static Scanner scan = new Scanner (System.in);
-    static storyline1 story= new storyline1();
+
+    static Scanner scan = new Scanner(System.in);
+    static storyline1 story = new storyline1();
     static String storybegin;
-    
-    public storyline1(){
+
+    public storyline1() {
         System.out.println("\t\t===============================\n"
                 + "\t\tWELCOME AND LET THE STORY BEGIN\n"
                 + "\t\t===============================\n");
-        
-        System.out.println("! POINT OF VIEW: reader as Sally Chou.\n");
+
         System.out.println("!!! Disclaimer: Cerita ini hanya fiksi belaka,\n"
                 + "tidak ada maksud terselubung dari cerita ini,\n"
                 + "dan mohon maaf apabila ada kesamaan nama/kejadian\n"
                 + "karena ini disetting sedemikian rupa agar menjadi cerita fiksi.\n\n");
-        
+
+        System.out.println("! POINT OF VIEW: reader as Sally Chou.\n");
+
         System.out.println("Pada suatu hari libur, ada anak cantik bernama Sally Chou.\n"
                 + "Ia tertidur pulas.\n"
                 + "Dalam mimpinya, ia bermimpi pergi ke suatu tempat luas.\n"
@@ -35,10 +36,9 @@ public class storyline1 {
                 + "Ruang kedua bertuliskan 'CS10'.");
         System.out.println("Apakah kamu mau ");
     }
-    
+
     public static void main(String[] args) {
-       
+
     }
-    
-    
+
 }
